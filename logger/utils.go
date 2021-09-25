9c89c12b1560ef1eebb564ffd4a42c3c4dcbec0c
Yurij Rubakha <yurij.osbb@gmail.com>
2021-09-25 15:07:28 +0300

    feat: StdTimeFormatter

--- logger/utils.go
+++ logger/utils.go
@@ -9,3 +9,39 @@ type ActualTimeProvider struct {
 func (a *ActualTimeProvider) GetCurrentTime() time.Time {
 	return time.Now().UTC()
 }
+
+type  CSVStringBuilder struct {
+	ColumnSeparator string
+	TimeFormatter
+}
+
+
+func (C CSVStringBuilder) MessageToString(message LogMessage) (string, error) {
+	var tmpStr string
+
+
+}
+
+
+type StdTimeFormatter struct {
+	DateComponentSeparator string
+	DateTimeSeparator string
+	TimeComponentSeparator string
+}
+
+func (stf *StdTimeFormatter) FormatTime(t time.Time) string {
+	var tmpStr string
+	y, m, d:=t.Date()
+	h, min, s:=t.Clock()
+	tmpStr = string(d) + stf.DateComponentSeparator +
+			string(m) + stf.DateComponentSeparator +
+			string(y) +stf.DateComponentSeparator +
+			stf.DateTimeSeparator +
+			string(h) +stf.TimeComponentSeparator +
+			string(min) +stf.TimeComponentSeparator +
+			string(s)
+	return tmpStr
+}
+
+
+
