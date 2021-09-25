1749681e3f006e92987d84dedeff38ef172e15b6
Anton Chubarov <achlp600@gmail.com>
2021-09-25 15:54:35 +0300

    test: tests and benchmarks

--- logger/utils.go
+++ logger/utils.go
@@ -6,46 +6,40 @@ import (
 )
 
 type ActualTimeProvider struct {
-
 }
 
 func (a *ActualTimeProvider) GetCurrentTime() time.Time {
 	return time.Now().UTC()
 }
 
-type  CSVStringBuilder struct {
+type CSVStringBuilder struct {
 	ColumnSeparator string
 	TimeFormatter
 }
 
-
-func (C CSVStringBuilder) MessageToString(message LogMessage) (string, error) {
-	var tmpStr string
-
-
-}
-
+//func (C CSVStringBuilder) MessageToString(message LogMessage) (string, error) {
+//	var tmpStr string
+//
+//
+//}
 
 type StdTimeFormatter struct {
 	format string
 }
 
 func NewStdTimeFormatter(
-		dateComponentSeparator string,
-		dateTimeSeparator string,
-		timeComponentSeparator string,
-	) *StdTimeFormatter {
+	dateComponentSeparator string,
+	dateTimeSeparator string,
+	timeComponentSeparator string,
+) *StdTimeFormatter {
 
 	return &StdTimeFormatter{
-			format: fmt.Sprintf("02%s01%s2006%s15%s04%s05", dateComponentSeparator, dateComponentSeparator,
-				dateTimeSeparator,
-				timeComponentSeparator, timeComponentSeparator),
-		}
+		format: fmt.Sprintf("02%s01%s2006%s15%s04%s05", dateComponentSeparator, dateComponentSeparator,
+			dateTimeSeparator,
+			timeComponentSeparator, timeComponentSeparator),
+	}
 }
 func (stf *StdTimeFormatter) FormatTime(t time.Time) string {
 
 	return t.Format(stf.format)
 }
-
-
-
